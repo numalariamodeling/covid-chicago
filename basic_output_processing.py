530a4d4dae801d3bcb451df436d1ee9240744511
Jaline Gerardin <jgerardin@northwestern.edu>
2020-03-20 10:49:49 -0500

    update basic params and remove waning reaction

--- basic_output_processing.py
+++ basic_output_processing.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 def reprocess(output_fname=None) :
 
-    fname = 'trajectories.csv'
+    sim_output_path = '/Users/jlg1657/Box/NU-malaria-team/projects/covid_chicago/cms_sim'
+    fname = os.path.join(sim_output_path, 'trajectories.csv')
     df = pd.read_csv(fname, skiprows=1)
     df = df.set_index('sampletimes').transpose()
     df = df.reset_index(drop=False)
