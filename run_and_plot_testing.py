7c9f59e94f28997249777fdff3f496ee049f9fbb
Garrett Eickelberg <geickelb@gmail.com>
2020-03-27 11:12:52 -0500

    rebase 03/27/20 (#3)
    * updates in r scripts
    
    * edits in basic plot output, added channels for daily newly events
    
    * added files for quick run + plotting (simple version only)
    
    * Revert "edits in basic plot output, added channels for daily newly events"
    
    This reverts commit 92d167c440a398c54736a048c6057bfd33a8b25b.
    
    * use base covid model as test model instead of base
    
    * added postprocessing compartments into cms model
    
    * renamed file
    
    * added running mutiple parameters and plot for the extended model
    
    * added observed detections in extended model
    
    * corrections in extended covid model
    
    * corrections extended model - detections
    
    * added d3 for detections of asymptomatic cases (currently set to 0)
    
    * corrections in extended model
    
    * added plot by facet for testing script
    
    * updated simple model configurations to run for 60 days
    
    * cleanup and gitignore
    
    * small cleanup to extended model emodl
    
    * revised the extended model sripts for parameter replacement and sampling
    
    * separate out recovered for each level of acuity
    
    * add detected bins
    
    Co-authored-by: ManuelaRunge <manuela.runge@northwestern.edu>
    Co-authored-by: Manuela Runge <ManuelaRunge@users.noreply.github.com>
    Co-authored-by: Jaline Gerardin <jgerardin@northwestern.edu>
    Co-authored-by: jgerardin <jgerardin@users.noreply.github.com>

--- run_and_plot_testing.py
+++ run_and_plot_testing.py
@@ -2,7 +2,8 @@ import os
 import subprocess
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import matplotlib.dates as mdates
+from datetime import date, timedelta
 
 ## directories
 user_path = os.path.expanduser('~')
@@ -12,11 +13,11 @@ if "mrung" in user_path :
     git_dir = os.path.join(user_path, 'gitrepos/covid-chicago/')
     sim_output_path = os.path.join(user_path, 'Box/NU-malaria-team/projects/covid_chicago/cms_sim')
 
-def runExp_simple() :
+def runExp_simple(modelname="extendedmodel_covid.emodl") :
 
     file = open('runModel_testing.bat', 'w')
     file.write('\n"' + os.path.join(exe_dir, "compartments.exe") + '"' + ' -c ' + '"' + os.path.join(git_dir, "simplemodel_testing.cfg") +
-                  '"' + ' -m ' + '"' + os.path.join( git_dir, "simplemodel_testing.emodl", ) + '"')
+                  '"' + ' -m ' + '"' + os.path.join( git_dir, modelname  ) + '"')
     file.close()
     subprocess.call([r'runModel_testing.bat'])
 
@@ -44,7 +45,6 @@ def reprocess(output_fname=None) :
 def plot(df) :
 
     plotchannels = [x for x in df.columns.values if 'time' not in x]
-    offset_channels = ['hospitalized', 'critical', 'death']
 
     fig = plt.figure()
     ax = fig.gca()
@@ -56,10 +56,36 @@ def plot(df) :
     plt.show()
 
 
+first_day = date(2020, 3, 1)
+
+def plot_by_channel(adf) :
+
+    fig = plt.figure(figsize=(8,6))
+    palette = sns.color_palette('Set1', 10)
+    allchannels = [x for x in df.columns.values if 'time' not in x]
+
+    axes = [fig.add_subplot(3,3,x+1) for x in range(len(allchannels))]
+    fig.subplots_adjust(bottom=0.05, hspace=0.25, right=0.95, left=0.1)
+    for c, channel in enumerate(allchannels) :
+
+        mdf = adf.groupby('time')[channel].agg([np.mean]).reset_index()
+        ax = axes[c]
+        #dates = [first_day + timedelta(days=int(x)) for x in mdf['time']]
+        ax.plot(df['time'], mdf['mean'], label=channel, color=palette[c])
+        ax.set_title(channel, y=0.8)
+
+        #formatter = mdates.DateFormatter("%m-%d")
+        #ax.xaxis.set_major_formatter(formatter)
+        #ax.xaxis.set_major_locator(mdates.MonthLocator())
+        #ax.set_xlim(first_day, )
+    plt.show()
+
 if __name__ == '__main__' :
 
-    runExp_simple()
+    runExp_simple(modelname="extendedmodel_covid.emodl")
+    runExp_simple(modelname="simplemodel_covid.emodl")
     df = reprocess()
     plot(df)
+    plot_by_channel(df)
 
 
