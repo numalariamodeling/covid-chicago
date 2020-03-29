7ad5cf14428702b2ff4c784f3469e83ad62a1128
ManuelaRunge <manuela.runge@northwestern.edu>
2020-03-29 16:13:25 -0500

    clean up test batch file

---
+++ runModel_testing.bat
@@ -0,0 +1,3 @@
+
+"C:/Users/mrung\Box\NU-malaria-team\projects\binaries\compartments\compartments.exe" -c "C:/Users/mrung\gitrepos\covid-chicago/simplemodel_testing.cfg" -m "C:/Users/mrung\gitrepos\covid-chicago/simplemodel_testing.emodl"
+pause
