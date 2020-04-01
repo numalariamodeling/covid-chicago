13d3c2c0a0a51c03a630e23a82654a412511d955
garrett <geickelb@github.com>
2020-04-01 12:05:06 -0500

    Update load_paths.py

--- load_paths.py
+++ load_paths.py
@@ -5,6 +5,7 @@ def load_box_paths(user_path=None):
 
     if not user_path :
         user_path = os.path.expanduser('~')
+<<<<<<< HEAD
         if 'garrett' in user_path:
             #user_path= 'C:/Users/geickelb1'
             ## idk where to put datapath since i don't have access to NU-malaria-team
@@ -31,6 +32,23 @@ def load_box_paths(user_path=None):
                 home_path =os.path.join(user_path,'Box')
                 #data_path = os.path.join(user_path, 'Box',  'data')
                 git_dir = os.path.join(user_path, 'Documents', 'Github', 'covid-chicago')
+=======
+        if 'jlg1657' in user_path :
+            user_path = 'E:/'
+            home_path = os.path.join(user_path, 'Box', 'NU-malaria-team','projects')
+            data_path = os.path.join(user_path, 'Box', 'NU-malaria-team', 'data')
+            #git_dir = os.path.join(user_path, 'Documents/covid-chicago/')
+        if 'mrung' in user_path :
+            user_path = 'C:/Users/mrung'
+            home_path = os.path.join(user_path, 'Box', 'NU-malaria-team','projects')
+            data_path = os.path.join(user_path, 'Box', 'NU-malaria-team', 'data')
+            git_dir = os.path.join(user_path, 'gitrepos', 'covid-chicago/')
+        if 'geickelb1' in user_path :
+            user_path = 'C:/Users/mrung'
+            home_path =os.path.join(user_path,'Box')
+            #data_path = os.path.join(user_path, 'Box',  'data')
+            git_dir = os.path.join(user_path, 'Documents', 'Github', 'covid-chicago')
+>>>>>>> parent of eed2ff3... update extended model postprocessing to include age groups (#9)
 
             project_path = os.path.join(home_path, 'covid_chicago')
             wdir = os.path.join(project_path, 'cms_sim')
