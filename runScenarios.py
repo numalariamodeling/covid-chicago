82256e3485ee545b2af5df40f0c8a5e7e39df90e
Arthi Ramachandran <4260771+arthir@users.noreply.github.com>
2020-04-15 16:09:51 -0500

    STY flaking, adding docstrings

--- runScenarios.py
+++ runScenarios.py
@@ -3,17 +3,15 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib as mpl
-from datetime import date, timedelta
-import shutil
+from datetime import date
 import sys
 import yaml
-import importlib
 
 from dotenv import load_dotenv
 
 from load_paths import load_box_paths
-from processing_helpers import *
-from simulation_helpers import DateToTimestep, makeExperimentFolder
+from simulation_helpers import (DateToTimestep, makeExperimentFolder, generateSubmissionFile,
+                                combineTrajectories, runExp, cleanup, sampleplot)
 from simulation_setup import load_setting_parameter
 
 log = logging.getLogger(__name__)
@@ -28,10 +26,14 @@ today = date.today()
 
 FUNCTIONS = {'uniform': np.random.uniform}
 
+NUMBER_OF_SAMPLES = 2
+NUMBER_OF_RUNS = 1
+DURATION = 365
+MONITORING_SAMPLES = 365  # needs to be smaller than duration
 
-# parameter samples
+
 def generateParameterSamples(samples, pop, first_day, config_name='./extendedcobey.yaml'):
-    """ Given a yaml configuration file (e.g. ./extendedcobey.yaml), 
+    """ Given a yaml configuration file (e.g. ./extendedcobey.yaml),
     generate a dataframe of the parameters for a simulation run using the specified 
     functions/sampling mechansims. 
     Supported functions are in the FUNCTIONS variable.
@@ -83,7 +85,7 @@ def replaceParameters(df, Ki_i, sample_nr, emodl_template, scen_num) :
         data = data.replace(f'@{col}@', str(df[col][sample_nr]))
     data = data.replace('@Ki@', '%.09f' % Ki_i)
     fin.close()
-    fin = open(os.path.join(temp_dir, f"simulation_{scen_num}emodl"), "wt")
+    fin = open(os.path.join(temp_dir, f"simulation_{scen_num}.emodl"), "wt")
     fin.write(data)
     fin.close()
 
@@ -99,28 +101,27 @@ def generateScenarios(simulation_population, Kivalues, duration, monitoring_samp
             scen_num += 1
 
             lst.append([sample, scen_num, i , first_day, simulation_population])
-            replaceParameters(df=dfparam, Ki_i=i, sample_nr= sample, emodl_template=modelname, scen_num=scen_num)
+            replaceParameters(df=dfparam, Ki_i=i, sample_nr=sample, emodl_template=modelname, scen_num=scen_num)
 
             # adjust model.cfg
-            fin = open(os.path.join(temp_exp_dir,"model.cfg"), "rt")
+            fin = open(os.path.join(temp_exp_dir, "model.cfg"), "rt")
             data_cfg = fin.read()
             data_cfg = data_cfg.replace('@duration@', str(duration))
             data_cfg = data_cfg.replace('@monitoring_samples@', str(monitoring_samples))
             data_cfg = data_cfg.replace('@nruns@', str(nruns))
             if not Location == 'Local':
-                data_cfg = data_cfg.replace('trajectories', 'trajectories_scen' + str(scen_num))
+                data_cfg = data_cfg.replace('trajectories', f'trajectories_scen{scen_num}')
             elif sys.platform not in ["win32", "cygwin"]:
                 # When running on Linux or OSX (and not in Quest), assume the
                 # trajectories directory is in the working directory.
-                traj_fname = os.path.join('trajectories',
-                                          'trajectories_scen' + str(scen_num))
+                traj_fname = os.path.join('trajectories', f'trajectories_scen{scen_num}')
                 data_cfg = data_cfg.replace('trajectories', traj_fname)
             elif Location == 'Local' :
-                data_cfg = data_cfg.replace('trajectories', './_temp/'+exp_name+'/trajectories/trajectories_scen' + str(scen_num))
+                data_cfg = data_cfg.replace('trajectories', f'./_temp/{exp_name}/trajectories/trajectories_scen{scen_num}')
             else:
                 raise RuntimeError("Unable to decide where to put the trajectories file.")
             fin.close()
-            fin = open(os.path.join(temp_dir,"model_"+str(scen_num)+".cfg"), "wt")
+            fin = open(os.path.join(temp_dir, "model_"+str(scen_num)+".cfg"), "wt")
             fin.write(data_cfg)
             fin.close()
 
@@ -155,11 +156,11 @@ if __name__ == '__main__' :
     custom_channel_list = ['detected_cumul', 'symp_severe_cumul', 'asymp_det_cumul', 'hosp_det_cumul',
                            'symp_mild_cumul', 'asymp_cumul', 'hosp_cumul', 'crit_cumul']
 
-    #============================================================
-    # Experiment design, fitting parameter and population
-    #=============================================================
+    # =============================================================
+    #   Experiment design, fitting parameter and population
+    # =============================================================
 
-    ### Define setting
+    # Define setting
     region = 'NMH_catchment'  # NMH_catchment  # IL  #EMS_3  # Chicago
     exp_name = today.strftime("%Y%m%d") + '_%s_updatedStartDate' % region + '_rn' + str(int(np.random.uniform(10, 99)))
 
@@ -176,31 +177,20 @@ if __name__ == '__main__' :
               f"sim_output_path = {sim_output_path}\n"
               f"plot_path = {plot_path}")
 
-    # function in simulation_setup.py
     populations, Kis, startdate = load_setting_parameter()
 
     simulation_population = populations[region]
-    number_of_samples = 2
-    number_of_runs = 1
-    duration = 365
-    monitoring_samples = 365  # needs to be smaller than duration
 
     # Parameter values
     Kivalues = Kis[region]
     first_day = startdate[region]
 
-    nscen = generateScenarios(simulation_population,
-                              Kivalues,
-                              nruns=number_of_runs,
-                              sub_samples=number_of_samples,
-                              duration = duration,
-                              monitoring_samples = monitoring_samples,
-                              modelname=emodl_template,
-                              first_day = first_day,
-                              Location=Location,
-                              )
-
-    '''
+    nscen = generateScenarios(
+        simulation_population, Kivalues,
+        nruns=NUMBER_OF_RUNS, sub_samples=NUMBER_OF_SAMPLES,
+        duration=DURATION, monitoring_samples=MONITORING_SAMPLES,
+        modelname=emodl_template, first_day=first_day, Location=Location)
+'''
     generateSubmissionFile(
         nscen, exp_name, trajectories_dir, temp_dir, temp_exp_dir,
         exe_dir=exe_dir, docker_image=docker_image)
@@ -209,13 +199,12 @@ if __name__ == '__main__' :
         runExp(trajectories_dir=trajectories_dir, Location='Local')
 
         # Once the simulations are done
-        #number_of_samples*len(Kivalues) == nscen ### to check
+        # number_of_samples*len(Kivalues) == nscen ### to check
         combineTrajectories(Nscenarios=nscen, trajectories_dir=trajectories_dir,
                             temp_exp_dir=temp_exp_dir, deleteFiles=False)
         cleanup(temp_exp_dir=temp_exp_dir, sim_output_path=sim_output_path,
                 plot_path=plot_path, delete_temp_dir=False)
         df = pd.read_csv(os.path.join(sim_output_path, 'trajectoriesDat.csv'))
-
 
         sampleplot(df, allchannels=master_channel_list, first_day=first_day,
                    plot_fname=os.path.join(plot_path, 'main_channels.png'))
@@ -223,4 +212,4 @@ if __name__ == '__main__' :
                    plot_fname=os.path.join('detection_channels.png'))
         sampleplot(df, allchannels=custom_channel_list, first_day=first_day,
                    plot_fname=os.path.join('cumulative_channels.png'))
-    '''
+'''
