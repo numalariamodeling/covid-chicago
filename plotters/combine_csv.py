bd8cfe4de2fe75f7c64c1586e3a18bb526a6eb9a
ManuelaRunge <manuela.runge@northwestern.edu>
2020-04-19 20:22:01 -0500

    modified combine.py to combine trajectoriesDat.csv and added ems input parameter plot (rfile)

--- plotters/combine_csv.py
+++ plotters/combine_csv.py
@@ -15,26 +15,24 @@ for scen in ['scenario1', 'scenario2', 'scenario3']:
     stem = scen
     exp_names = [x for x in os.listdir(os.path.join(wdir, 'simulation_output')) if stem in x]
 
-    channels = ['date', "infected_median", "infected_95CI_lower", "infected_95CI_upper",
-                "deaths_median", "deaths_95CI_lower","deaths_95CI_upper",
-                "hospitalized_median", "hospitalized_95CI_lower", "hospitalized_95CI_upper",
-                "critical_median", "critical_95CI_lower", "critical_95CI_upper",
-                "ventilators_median", "ventilators_95CI_lower", "ventilators_95CI_upper"]
-
     adf = pd.DataFrame()
     for d, exp_name in enumerate(exp_names):
 
         sim_output_path = os.path.join(wdir, 'simulation_output', exp_name)
         ems = int(exp_name.split('_')[2])
-        df = pd.read_csv(os.path.join(sim_output_path, 'projection_for_civis.csv'))
+        df = pd.read_csv(os.path.join(sim_output_path, 'trajectoriesDat.csv'))
+
+        first_day = datetime.strptime(cdf['first_day'].unique()[0], '%Y-%m-%d')
+
         df['ems'] = ems
-        adf = pd.concat([adf, df[channels + ['ems']]])
+        df['date'] = df['time'].apply(lambda x: first_day + timedelta(days=int(x)))
+        adf = pd.concat([adf, df])
 
     if scen == 'scenario1' :
-        filename = 'nu_EMS_endsip_20200418.csv'
+        filename = 'EMS_trajectories_separate_endsip_20200419.csv'
     if scen == 'scenario2':
-        filename = 'nu_EMS_neversip_20200418.csv'
+        filename = 'EMS_trajectories_neversip_20200419.csv'
     if scen == 'scenario3':
-        filename = 'nu_EMS_baseline_20200418.csv'
+        filename = 'EMS_trajectories_baseline_20200419.csv'
 
     adf.to_csv(os.path.join(wdir,'simulation_output/_csv', filename), index=False)
