615ce63631d23867341237440f0bff11972c506c
ManuelaRunge <manuela.runge@northwestern.edu>
2020-04-20 16:04:05 -0500

    moved outdated /not used  files into archive folder to be deleted

--- runModel_testing.bat
+++
@@ -1,2 +0,0 @@
-
-"C:/Users/mrung\Box\NU-malaria-team\projects\binaries\compartments\compartments.exe" -c "C:/Users/mrung/gitrepos/covid-chicago/cfg/simplemodel_testing.cfg" -m "C:/Users/mrung\gitrepos\covid-chicago/emodl/simplemodel_testing.emodl"
