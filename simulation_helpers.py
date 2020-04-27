1056522991a8e371bb091c862a3c0ea9deec6015
ManuelaRunge <manuela.runge@northwestern.edu>
2020-04-27 16:22:25 -0500

    add experiment_confif to path in function and also copy  the yaml file to the experiment folder (for reproducibility)

--- simulation_helpers.py
+++ simulation_helpers.py
@@ -214,7 +214,7 @@ echo end""")
     file.close()
 
 
-def makeExperimentFolder(exp_name, emodl_dir, emodlname, cfg_dir, temp_exp_dir=None,
+def makeExperimentFolder(exp_name, emodl_dir, emodlname, cfg_dir, yaml_dir, experiment_config, temp_exp_dir=None,
                          wdir=WDIR, git_dir=GIT_DIR): ## GE 04/10/20 added exp_name, emodl_dir,emodlname, cfg_dir here to fix exp_name not defined error
     sim_output_path = os.path.join(wdir, 'simulation_output', exp_name)
     plot_path = sim_output_path
@@ -236,6 +236,7 @@ def makeExperimentFolder(exp_name, emodl_dir, emodlname, cfg_dir, temp_exp_dir=N
     ## Copy emodl and cfg file  to experiment folder
     shutil.copyfile(os.path.join(emodl_dir, emodlname), os.path.join(temp_exp_dir, emodlname))
     shutil.copyfile(os.path.join(cfg_dir, 'model.cfg'), os.path.join(temp_exp_dir, 'model.cfg'))
+    shutil.copyfile(os.path.join(yaml_dir, experiment_config), os.path.join(temp_exp_dir, experiment_config))
 
     return temp_dir, temp_exp_dir, trajectories_dir, sim_output_path, plot_path
 
