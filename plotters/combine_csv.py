95a0fc7426e772ed921c4335d23d4daf543d1f17
ManuelaRunge <manuela.runge@northwestern.edu>
2020-04-30 19:55:39 -0500

    updated combine.csv  to aggregate run_number and save file in specified output_dir

--- plotters/combine_csv.py
+++ plotters/combine_csv.py
@@ -9,29 +9,35 @@ from data_comparison import load_sim_data
 
 datapath, projectpath, wdir,exe_dir, git_dir = load_box_paths()
 
+input_dir = os.path.join(wdir, 'simulation_output/')
+output_dir = os.path.join(projectpath,'NU_civis_outputs','20200429','trajectories')
 
 for scen in ['scenario1', 'scenario2', 'scenario3']:
     stem = scen
-    exp_names = [x for x in os.listdir(os.path.join(wdir, 'simulation_output')) if stem in x]
+    exp_names = [x for x in os.listdir(input_dir) if stem in x]
 
     adf = pd.DataFrame()
     for d, exp_name in enumerate(exp_names):
 
-        sim_output_path = os.path.join(wdir, 'simulation_output', exp_name)
+        sim_output_path = os.path.join(input_dir, exp_name)
         ems = int(exp_name.split('_')[2])
-        df = pd.read_csv(os.path.join(sim_output_path, 'trajectoriesDat.csv'))
+        df = pd.read_csv(os.path.join(sim_output_path, 'trajectoriesDat_test.csv'))
 
-        first_day = datetime.strptime(cdf['first_day'].unique()[0], '%Y-%m-%d')
+        first_day = datetime.strptime(df['first_day'].unique()[0], '%Y-%m-%d')
 
         df['ems'] = ems
         df['date'] = df['time'].apply(lambda x: first_day + timedelta(days=int(x)))
+
+        df = df.groupby(['ems','date','time','sample_num','scen_num']).agg(np.mean).reset_index()
+
         adf = pd.concat([adf, df])
 
     if scen == 'scenario1' :
-        filename = 'EMS_trajectories_separate_endsip_20200419.csv'
+        filename = 'Trajectories_endsip.csv'
     if scen == 'scenario2':
-        filename = 'EMS_trajectories_neversip_20200419.csv'
+        filename = 'Trajectories_neversip.csv'
     if scen == 'scenario3':
-        filename = 'EMS_trajectories_baseline_20200419.csv'
+        filename = 'Trajectories_baseline.csv'
 
-    adf.to_csv(os.path.join(wdir,'simulation_output/_csv', filename), index=False)
+    adf.to_csv(os.path.join(output_dir, filename), index=False)
+
