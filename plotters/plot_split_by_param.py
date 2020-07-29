c71c60580f2ff1e6036a31a41806e310b4f250d5
ManuelaRunge <manuela.runge@northwestern.edu>
2020-07-28 19:00:38 -0500

    iteration comparison per covid region

--- plotters/plot_split_by_param.py
+++ plotters/plot_split_by_param.py
@@ -36,16 +36,22 @@ def plot_on_fig(df, channels, axes, color, label) :
         ax.xaxis.set_major_formatter(formatter)
         ax.xaxis.set_major_locator(mdates.MonthLocator())
 
+def plot_on_fig2(df, c, axes,channel, color,panel_heading, label) :
+    ax = axes[c]
+    mdf = df.groupby('time')[channel].agg([CI_50, CI_2pt5, CI_97pt5, CI_25, CI_75]).reset_index()
 
-if __name__ == '__main__' :
+    mdf['date'] = mdf['time'].apply(lambda x: first_day + timedelta(days=int(x)))
+    mdf = mdf[(mdf['date'] >= date(2020, 5, 1)) & (mdf['date'] <= date(2020, 10, 1))]
+    ax.plot(mdf['date'], mdf['CI_50'], color=color, label=label)
+    ax.fill_between(mdf['date'].values, mdf['CI_25'], mdf['CI_75'],
+                color=color, linewidth=0, alpha=0.4)
+    ax.set_title(panel_heading, y=0.85)
+    formatter = mdates.DateFormatter("%m-%d")
+    ax.xaxis.set_major_formatter(formatter)
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
 
-    exp_names = ['20200430_EMS_11_RR_crit1to3',
-                 '20200430_EMS_11_RR_crit4to6']
-    capacity = {
-        'hospitalized' : 0,
-        'critical' : 0
-    }
 
+def plot_main() :
     fig = plt.figure(figsize=(8, 8))
     fig.subplots_adjust(right=0.97, wspace=0.2, left=0.1, hspace=0.25, top=0.95, bottom=0.07)
     palette = sns.color_palette('Set1', len(exp_names))
@@ -61,13 +67,82 @@ if __name__ == '__main__' :
 
         plot_on_fig(df, channels, axes, color=palette[d], label=exp_name)
     axes[-1].legend()
-    for c, channel in enumerate(channels) :
-        if channel in capacity.keys() :
-            ax = axes[c]
-            ax.plot([date(2020, 5, 1), date(2020, 10, 1)],
-                    [capacity[channel], capacity[channel]], '--', linewidth=2, color='k')
 
     plt.savefig(os.path.join(sim_output_path, 'projection.png'))
     plt.savefig(os.path.join(sim_output_path, 'projection.pdf'), format='PDF')
-    plt.show()
+    #plt.show()
+
+def plot_subgroups() :
+
+    subgroups = ['_EMS-1', '_EMS-2', '_EMS-3', '_EMS-4', '_EMS-5', '_EMS-6', '_EMS-7', '_EMS-8', '_EMS-9',
+                 '_EMS-10', '_EMS-11']
+
+    for region_suffix in subgroups :
+
+        region_label= region_suffix.replace('_EMS-', 'covid region ')
+        region_label2 = region_label.replace(' ', '_')
+
+        fig = plt.figure(figsize=(8, 8))
+        fig.subplots_adjust(right=0.97, wspace=0.2, left=0.1, hspace=0.25, top=0.95, bottom=0.07)
+        palette = sns.color_palette('Set1', len(exp_names))
+        channels = ['infected', 'new_detected', 'new_deaths', 'hospitalized', 'critical', 'ventilators']
+        axes = [fig.add_subplot(3, 2, x + 1) for x in range(len(channels))]
+
+
+        for d, exp_name in enumerate(exp_names) :
+            sim_output_path = os.path.join(wdir, 'simulation_output', exp_name)
+            df = load_sim_data(exp_name, region_suffix=region_suffix)
+
+            df['symptomatic_census'] = df['symptomatic_mild'] + df['symptomatic_severe']
+            df['ventilators'] = get_vents(df['crit_det'].values)
+
+            plot_on_fig(df, channels, axes, color=palette[d], label=exp_name)
+
+        axes[-1].legend()
+        fig.suptitle(region_label)
+        plt.savefig(os.path.join(sim_output_path, 'iteration_comparison_%s.png' % region_label2))
+        #plt.savefig(os.path.join(sim_output_path, 'iteration_comparison_%s.pdf' % region_label2))
+        #plt.show()
+
+
+def plot_subgroups_inone(channel='hospitalized') :
+
+    subgroups = ['_EMS-1', '_EMS-2', '_EMS-3', '_EMS-4', '_EMS-5', '_EMS-6', '_EMS-7', '_EMS-8', '_EMS-9',
+                 '_EMS-10', '_EMS-11']
+
+    fig = plt.figure(figsize=(12, 8))
+    fig.subplots_adjust(right=0.97, wspace=0.2, left=0.1, hspace=0.25, top=0.95, bottom=0.07)
+    palette = sns.color_palette('Set1', len(exp_names))
+    axes = [fig.add_subplot(4, 3, x + 1) for x in range(len(subgroups))]
+
+    for c, region_suffix in enumerate(subgroups) :
+
+        region_label= region_suffix.replace('_EMS-', 'covid region ')
+        region_label2 = region_label.replace(' ', '_')
+
+        for d, exp_name in enumerate(exp_names) :
+            sim_output_path = os.path.join(wdir, 'simulation_output', exp_name)
+            df = load_sim_data(exp_name, region_suffix=region_suffix)
+
+            plot_on_fig2(df, c, axes, channel=channel, color=palette[d],panel_heading = region_label,  label=exp_name)
+
+        axes[-1].legend()
+        fig.suptitle(x=0.5, y=0.999,t=channel)
+        plt.tight_layout()
+        plt.savefig(os.path.join(sim_output_path, 'iteration_comparison_2_%s.png' % channel))
+        #plt.savefig(os.path.join(sim_output_path, 'iteration_comparison_2_%s.pdf' % channel))
+        #plt.show()
+
+
+
+if __name__ == '__main__' :
+
+    exp_names = ['20200722_IL_RR_fitting_8',
+                 '20200728_IL_RR_fitting_3']
+
+    plot_main()
+    #plot_subgroups()
+    plot_subgroups_inone(channel='hospitalized')
+
+
 
