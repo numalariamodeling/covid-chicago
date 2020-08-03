58837ef2d504223acf932760697fccbbfd278b71
ManuelaRunge <manuela.runge@northwestern.edu>
2020-08-03 18:13:20 -0500

    updated fitted param plot and moved to subfolder

--- Rfiles/simulation_plotter/fitted_param_plots.R
+++ Rfiles/simulation_plotter/fitted_param_plots.R
@@ -1,8 +1,15 @@
 
 library(tidyverse)
+library(cowplot)
+library(scales)
+library(viridis)
+
+source("load_paths.R")
+source("processing_helpers.R")
+
 
 f_plot_param <- function(exp_name, paramname, emsname, timeVarying = TRUE, SAVE = TRUE) {
-  trajectoriesDat <- read.csv(file.path(simulation_output, exp_name, "trajectoriesDat.csv"))
+  trajectoriesDat <- read.csv(file.path(simulation_output,exp_name, "trajectoriesDat.csv"))
 
   ### Note use Ki_EMS-  or Ki_EMS.   not Ki_EMS_  (time varying vs input parameter)
   colnames(trajectoriesDat) <- gsub("[.]", "-", colnames(trajectoriesDat))
@@ -30,8 +37,9 @@ f_plot_param <- function(exp_name, paramname, emsname, timeVarying = TRUE, SAVE 
       geom_bar(aes(x = as.factor(region), y = value, group = region), stat = "identity", size = 1.3, position = "dodge") +
       scale_color_viridis(discrete = TRUE) +
       labs(
-        y = paramname,
-        title = exp_name,
+        y = gsub("_"," ",paramname), #"% relaxation"
+        title = "",
+        #subtitle = "Estimated relaxation of shelter-in-place polices\n (reopening 21st June)",
         subtitle = "",
         x = "region"
       ) +
@@ -50,16 +58,17 @@ f_plot_param <- function(exp_name, paramname, emsname, timeVarying = TRUE, SAVE 
       ) %>%
       group_by(date, region,exp_name) %>%
       summarize(value = mean(value)) %>%
-      filter(date <= as.Date("2020-08-02")) %>%
+      filter(date <= as.Date("2020-09-02")) %>%
       ggplot() +
       theme_cowplot() +
-      geom_bar(aes(x = date, y = value, col = region, group = region), stat = "identity", size = 1.3, position = "dodge") +
+      geom_line(aes(x = date, y = value, col = as.factor(region), group = region), size = 1.3) +
       scale_color_viridis(discrete = TRUE) +
       labs(
-        y = paramname,
-        title = exp_name,
-        subtitle = "",
-        x = "date"
+        y = gsub("_"," ",paramname),
+        subtitle = "", # Estimated change in transmission intensity\n
+        title = "",
+        x = "",
+        col="covid region"
       ) +
       customThemeNoFacet +
       scale_y_continuous(expand = c(0, 0))
@@ -67,7 +76,10 @@ f_plot_param <- function(exp_name, paramname, emsname, timeVarying = TRUE, SAVE 
 
   if (SAVE) {
     ggsave(paste0(paramname, ".png"),
-      plot = pplot, path = file.path(simulation_output, exp_name), width = 10, height = 5, device = "png"
+      plot = pplot, path = file.path(simulation_output, exp_name), width = 8, height = 5, device = "png"
+    )
+    ggsave(paste0(paramname, ".pdf"),
+           plot = pplot, path = file.path(simulation_output, exp_name), width = 8, height = 5, device = "pdf"
     )
   }
 
@@ -76,13 +88,17 @@ f_plot_param <- function(exp_name, paramname, emsname, timeVarying = TRUE, SAVE 
 }
 
 
+
 ### Load trajectories Dat
-exp_name <- "20200722_IL_EMS_scen3"
-#exp_name <- "20200729_IL_RR_baseline_0"
+exp_name <- "20200730_IL_MR_improvedSym_all0.5"
+
+#simulation_output <-  file.path(simulation_output, "EMS/20200730_increasedSym/")
 
 
-f_plot_param(exp_name = exp_name, paramname =  "backtonormal_multiplier_1_", emsname="EMS_", timeVarying = FALSE)
+f_plot_param(exp_name = exp_name, paramname = "social_multiplier_4_", emsname="EMS_", timeVarying = FALSE)
+f_plot_param(exp_name = exp_name, paramname = "backtonormal_multiplier_1_", emsname="EMS_", timeVarying = FALSE)
+f_plot_param(exp_name = exp_name, paramname = "Ki_t_", emsname = "EMS_", timeVarying = TRUE)
+f_plot_param(exp_name = exp_name, paramname = "d_Sym_t_", emsname = "EMS_", timeVarying = TRUE)
 
-f_plot_param(exp_name = exp_name, paramname = "Ki_", emsname = "EMS-", timeVarying = TRUE)
 
 
