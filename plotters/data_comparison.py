a21891a49a7cb38e73ffbdf3a9d1b715342f64af
Manuela Runge <ManuelaRunge@users.noreply.github.com>
2020-08-17 08:20:39 -0500

    added plot_split_by_param for plotting single trajectories , changed '_' to '-' for Ki dSym observed emodl output (#445)
    plot_split_by_param_trajectories.py
    * added plot_split_by_param for plotting unaggregated trajectories and allowed  to define fname in load_sim_data
    * for covid regions, also show changes in Ki and dSym
    * plot full timeline
    emodl
    * use 'grpout' for observed group parameters (for easier filtering in postprocessing)
    * added updated emodls
    plot_split_by_channel.py
    * added _detected  to sub command and adjusted figure size and filename
    plot_fitted_param.R
    * updated plot fitted_param r script to use "-" for Ki, dSym,  added Location to load path if not defined

--- plotters/data_comparison.py
+++ plotters/data_comparison.py
@@ -18,12 +18,12 @@ datetoday = date(today.year, today.month, today.day)
 
 datapath, projectpath, wdir,exe_dir, git_dir = load_box_paths()
 
-def load_sim_data(exp_name, region_suffix ='_All', input_wdir=None, input_sim_output_path =None) :
+def load_sim_data(exp_name, region_suffix ='_All', input_wdir=None,fname='trajectoriesDat.csv', input_sim_output_path =None) :
     input_wdir = input_wdir or wdir
     sim_output_path_base = os.path.join(input_wdir, 'simulation_output', exp_name)
     sim_output_path = input_sim_output_path or sim_output_path_base
 
-    df = pd.read_csv(os.path.join(sim_output_path, 'trajectoriesDat.csv'))
+    df = pd.read_csv(os.path.join(sim_output_path, fname))
     if 'Ki' not in df.columns.values :
         scen_df = pd.read_csv(os.path.join(sim_output_path, 'sampled_parameters.csv'))
         df = pd.merge(left=df, right=scen_df[['scen_num', 'Ki']], on='scen_num', how='left')
