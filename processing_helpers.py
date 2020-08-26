719f1d9fcf09c8883a6a719413432e01012d1a33
ManuelaRunge <manuela.runge@northwestern.edu>
2020-08-26 14:55:22 -0500

    corrected file path

--- processing_helpers.py
+++ processing_helpers.py
@@ -133,7 +133,7 @@ def load_capacity(ems, simdate='20200825') :
     ### note, names need to match, simulations and capacity data already include outputs for all illinois
     
     fname = 'capacity_by_covid_region_' + simdate + '.csv'
-    ems_fname = os.path.join(datapath, 'covid_IDPH/Corona virus reports', fname)
+    ems_fname = os.path.join(datapath, 'covid_IDPH/Corona virus reports/hospital_capacity_thresholds_template/', fname)
     ems_df = pd.read_csv(ems_fname)
 
     filterdate = max(ems_df['date'])
