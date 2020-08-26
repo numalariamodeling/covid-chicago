a026a00bd94ece605b8d5044c6d018a104349e76
Manuela Runge <ManuelaRunge@users.noreply.github.com>
2020-08-25 22:33:20 -0500

    Updated py files using 'usecol' and covidregion instead of EMS in plotnames (#456)
    * revised trim_trajectories using 'usecols'
    * updated EMS to covidregion in plot filenames
    * removed not required lines in data_comparison_spatial and added usecols for faster processing
    * updated variable name for 'recovered' to match pattern of other variables (output file for civis)

--- plotters/trim_trajectoriesDat.py
+++ plotters/trim_trajectoriesDat.py
@@ -1,43 +1,54 @@
 import os
 import pandas as pd
 import sys
+
 sys.path.append('../')
 from load_paths import load_box_paths
-
 datapath, projectpath, wdir,exe_dir, git_dir = load_box_paths()
+sim_output_path = os.path.join(wdir, "simulation_output")
 
-def trim_trajectories_Dat(exp_name, additionalVarsToKeep, keepTimes=None) :
+
+def trim_trajectories_Dat(exp_dir, VarsToKeep, keepTimes=None):
     """Generate a subset of the trajectoriesDat dataframe
     The new csv file is saved under trajectoriesDat_trim.csv, no dataframe is returned
-    Parameters
-    ----------
-    exp_name : str - name of the experiment
-    additionalVarsToKeep : list - column names to keep in addition to the main channels 
-    keepTimes : int - minimum timestep to keep (all values below are discarded)
     """
 
-    sim_output_path = os.path.join(wdir, 'simulation_output', exp_name)
-    df = pd.read_csv(os.path.join(sim_output_path, 'trajectoriesDat.csv'))
-    
-    keepvars = ['time','startdate','scen_num','run_num','sample_num']
-    keepvars = keepvars + [x for x in df.columns.values if 'EMS-' in x]
-    keepvars = keepvars + [x for x in df.columns.values if 'All' in x]
-    keepvars = keepvars + additionalVarsToKeep
+    column_list = VarsToKeep
+    for ems_region in range(1, 12):
+        column_list.append('infected_EMS-' + str(ems_region))
+        column_list.append('infectious_undet_EMS-' + str(ems_region))
+        column_list.append('infectious_det_EMS-' + str(ems_region))
+        column_list.append('prevalence_EMS-' + str(ems_region))
+        column_list.append('recovered_EMS-' + str(ems_region))
+        column_list.append('hospitalized_EMS-' + str(ems_region))
+        column_list.append('hospitalized_det_EMS-' + str(ems_region))
+        column_list.append('critical_EMS-' + str(ems_region))
+        column_list.append('critical_det_EMS-' + str(ems_region))
+        column_list.append('crit_det_EMS-' + str(ems_region))
+        column_list.append('deaths_det_EMS-' + str(ems_region))
+        column_list.append('deaths_EMS-' + str(ems_region))
+        column_list.append('Ki_t_EMS-' + str(ems_region))
 
-    df = df[keepvars]
+    df = pd.read_csv(os.path.join(exp_dir, 'trajectoriesDat.csv'), usecols=column_list)
 
-    if keepTimes is not None :
-        df = df[df['time']>=int(keepTimes)]
+    if keepTimes is not None:
+        df = df[df['time'] >= int(keepTimes)]
 
-    df.to_csv(os.path.join(sim_output_path, 'trajectoriesDat_trim.csv'), index=False)
+    df.to_csv(os.path.join(exp_dir, 'trajectoriesDat_trim.csv'), index=False)
+
 
 if __name__ == '__main__':
-    #stem = sys.argv[1]
-    stem = "20200805"
-    exp_names = [x for x in os.listdir(os.path.join(wdir, 'simulation_output')) if stem in x]
-    #exp_name = "20200805_IL_rollback_Sep30"
 
-    additionalVarsToKeep = ['reopening_multiplier_4']
+    VarsToKeep = ['startdate', 'time', 'scen_num','sample_num', 'run_num', 'infectious_undet_All',
+                            'infectious_det_All', 'recovered_All', 'prevalence_All', 'infected_All', 'deaths_All',
+                            'deaths_det_All', 'hospitalized_det_All', 'crit_det_All', 'hospitalized_All',
+                            'critical_All', 'reopening_multiplier_4', 'reduced_inf_of_det_cases_ct1',
+                            'change_testDelay_Sym_1', 'change_testDelay_As_1', 'd_Sym_ct1', 'd_AsP_ct1']
+
+    stem = sys.argv[1]
+    #stem ="20200722_IL_EMS_scen3"
+    exp_names = [x for x in os.listdir(os.path.join(sim_output_path)) if stem in x]
 
     for exp_name in exp_names:
-        trim_trajectories_Dat(exp_name=exp_name, additionalVarsToKeep=additionalVarsToKeep, keepTimes=120)
+        exp_dir = os.path.join(sim_output_path, exp_name)
+        trim_trajectories_Dat(exp_dir=exp_dir, VarsToKeep=VarsToKeep, keepTimes=120)
