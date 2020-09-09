e11f15ed592c2754a52fbed23800efaee0dfe59d
ManuelaRunge <manuela.runge@northwestern.edu>
2020-09-08 21:02:58 -0500

    updated load path to clearly separate between cluster and local use despite same usernames (for some)

--- load_paths.py
+++ load_paths.py
@@ -12,33 +12,6 @@ def load_box_paths(user_path=None, Location ='Local'):
     exe_dir = os.getenv("EXE_DIR")
     git_dir = os.getenv("GIT_DIR")
 
-    if not user_path :
-        user_path = os.path.expanduser('~')
-
-    if 'garrett' in user_path:
-        #user_path= 'C:/Users/geickelb1'
-        ## idk where to put datapath since i don't have access to NU-malaria-team
-        data_path= os.path.join(user_path, 'Box',  'data')
-        project_path= os.path.join(user_path,'Box','covid_chicago') #was origionally project_path
-        wdir= os.path.join(user_path,'Box','covid_chicago','cms_sim')
-        exe_dir = os.path.join(user_path, 'Box','compartments')
-        git_dir = os.path.join(user_path, 'Documents','GitHub','covid-chicago')
-
-    if 'patri' in user_path:
-        data_path= os.path.join(user_path, 'Box Sync')
-        project_path= os.path.join(user_path,'Box Sync','covid_chicago') #was origionally project_path
-        wdir= os.path.join(user_path,'Box Sync','covid_chicago','cms_sim')
-        exe_dir = os.path.join(user_path, 'Box Sync','compartments')
-        git_dir = os.path.join(user_path, 'Documents','GitHub','covid-chicago')
-        
-    if 'tmh6260' in user_path:
-        data_path = os.path.join(user_path, 'Box Sync')
-        home_path = os.path.join(user_path, 'Box', 'NU-malaria-team', 'projects') # had an error so I added this
-        project_path = os.path.join(user_path, 'Box Sync', 'covid_chicago')  #
-        wdir = os.path.join(user_path, 'Box Sync', 'covid_chicago', 'cms_sim')
-        exe_dir = os.path.join(user_path, 'Box Sync', 'compartments')
-        git_dir = os.path.join(user_path, 'Documents', 'GitHub', 'covid-chicago')
-
     if Location == 'NUCLUSTER':
         user_path = '/projects/p30781/'
         home_path = os.path.join(user_path, 'covidproject', 'projects')
@@ -48,33 +21,62 @@ def load_box_paths(user_path=None, Location ='Local'):
         wdir = os.path.join(project_path, 'cms_sim')
         exe_dir = os.path.join(home_path, 'binaries', 'compartments')
         
-    else:
-
-        if 'jlg1657' in user_path :
-            git_dir = os.path.join('C:/Users/jlg1657', 'Documents/covid-chicago/')
-            # user_path = 'E:/'
-            home_path = os.path.join(user_path, 'Box', 'NU-malaria-team','projects')
-            data_path = os.path.join(user_path, 'Box', 'NU-malaria-team', 'data')
-        if 'mrung' in user_path or 'mrm9534' in user_path  :
-            user_path = user_path
-            home_path = os.path.join(user_path, 'Box', 'NU-malaria-team','projects')
-            data_path = os.path.join(user_path, 'Box', 'NU-malaria-team', 'data')
-            git_dir = os.path.join(user_path, 'gitrepos', 'covid-chicago/')
-        if 'geickelb1' in user_path :
-            #user_path = 'C:/Users/geickelb1'
-            home_path =os.path.join(user_path,'Box')
-            #data_path = os.path.join(user_path, 'Box',  'data')
-            git_dir = os.path.join(user_path, 'Documents', 'Github', 'covid-chicago')
-        if 'Ibis' in user_path:
-            #user_path = r'\~/'
+    if Location == 'Local':
+        
+        if not user_path :
+            user_path = os.path.expanduser('~')
+    
+        if 'garrett' in user_path:
+            #user_path= 'C:/Users/geickelb1'
+            ## idk where to put datapath since i don't have access to NU-malaria-team
+            data_path= os.path.join(user_path, 'Box',  'data')
+            project_path= os.path.join(user_path,'Box','covid_chicago') #was origionally project_path
+            wdir= os.path.join(user_path,'Box','covid_chicago','cms_sim')
+            exe_dir = os.path.join(user_path, 'Box','compartments')
+            git_dir = os.path.join(user_path, 'Documents','GitHub','covid-chicago')
+    
+        if 'patri' in user_path:
+            data_path= os.path.join(user_path, 'Box Sync')
+            project_path= os.path.join(user_path,'Box Sync','covid_chicago') #was origionally project_path
+            wdir= os.path.join(user_path,'Box Sync','covid_chicago','cms_sim')
+            exe_dir = os.path.join(user_path, 'Box Sync','compartments')
+            git_dir = os.path.join(user_path, 'Documents','GitHub','covid-chicago')
+            
+        if 'tmh6260' in user_path:
+            data_path = os.path.join(user_path, 'Box Sync')
+            home_path = os.path.join(user_path, 'Box', 'NU-malaria-team', 'projects') # had an error so I added this
+            project_path = os.path.join(user_path, 'Box Sync', 'covid_chicago')  #
+            wdir = os.path.join(user_path, 'Box Sync', 'covid_chicago', 'cms_sim')
+            exe_dir = os.path.join(user_path, 'Box Sync', 'compartments')
             git_dir = os.path.join(user_path, 'Documents', 'GitHub', 'covid-chicago')
-            home_path = os.path.join(user_path, 'Box')
-            data_path = os.path.join(user_path, 'Box')
-            #project_path = os.path.join(home_path, 'Box', 'covid_chicago')
-
-        project_path = os.path.join(home_path, 'covid_chicago')
-        wdir = os.path.join(project_path, 'cms_sim')
-        exe_dir = os.path.join(home_path, 'binaries', 'compartments')
+        
+        else:
+    
+            if 'jlg1657' in user_path :
+                git_dir = os.path.join('C:/Users/jlg1657', 'Documents/covid-chicago/')
+                # user_path = 'E:/'
+                home_path = os.path.join(user_path, 'Box', 'NU-malaria-team','projects')
+                data_path = os.path.join(user_path, 'Box', 'NU-malaria-team', 'data')
+            if 'mrung' in user_path or 'mrm9534' in user_path  :
+                user_path = user_path
+                home_path = os.path.join(user_path, 'Box', 'NU-malaria-team','projects')
+                data_path = os.path.join(user_path, 'Box', 'NU-malaria-team', 'data')
+                git_dir = os.path.join(user_path, 'gitrepos', 'covid-chicago/')
+            if 'geickelb1' in user_path :
+                #user_path = 'C:/Users/geickelb1'
+                home_path =os.path.join(user_path,'Box')
+                #data_path = os.path.join(user_path, 'Box',  'data')
+                git_dir = os.path.join(user_path, 'Documents', 'Github', 'covid-chicago')
+            if 'Ibis' in user_path:
+                #user_path = r'\~/'
+                git_dir = os.path.join(user_path, 'Documents', 'GitHub', 'covid-chicago')
+                home_path = os.path.join(user_path, 'Box')
+                data_path = os.path.join(user_path, 'Box')
+                #project_path = os.path.join(home_path, 'Box', 'covid_chicago')
+    
+            project_path = os.path.join(home_path, 'covid_chicago')
+            wdir = os.path.join(project_path, 'cms_sim')
+            exe_dir = os.path.join(home_path, 'binaries', 'compartments')
 
     return data_path, project_path, wdir , exe_dir, git_dir
 
