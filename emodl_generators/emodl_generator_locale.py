09197fe8051ad1a97063b89ae0da7a07b6ec196b
ArielleEagan <arielle.eagan@northwestern.edu>
2020-11-30 20:16:55 -0600

    20201130 added transmission multiplier 12

--- emodl_generators/emodl_generator_locale.py
+++ emodl_generators/emodl_generator_locale.py
@@ -880,6 +880,7 @@ def write_interventions(grpList, total_string, scenarioName, change_testDelay=No
 (param Ki_red9_{grp} (* Ki_{grp} @ki_multiplier_9_{grp}@))
 (param Ki_red10_{grp} (* Ki_{grp} @ki_multiplier_10_{grp}@))
 (param Ki_red11_{grp} (* Ki_{grp} @ki_multiplier_11_{grp}@))
+(param Ki_red12_{grp} (* Ki_{grp} @ki_multiplier_12_{grp}@))
 
 (time-event ki_multiplier_change_3a @ki_multiplier_time_3a@ ((Ki_{grp} Ki_red3a_{grp})))
 (time-event ki_multiplier_change_3b @ki_multiplier_time_3b@ ((Ki_{grp} Ki_red3b_{grp})))
@@ -892,6 +893,7 @@ def write_interventions(grpList, total_string, scenarioName, change_testDelay=No
 (time-event ki_multiplier_change_9 @ki_multiplier_time_9@ ((Ki_{grp} Ki_red9_{grp})))
 (time-event ki_multiplier_change_10 @ki_multiplier_time_10@ ((Ki_{grp} Ki_red10_{grp})))
 (time-event ki_multiplier_change_11 @ki_multiplier_time_11@ ((Ki_{grp} Ki_red11_{grp})))
+(time-event ki_multiplier_change_12 @ki_multiplier_time_12@ ((Ki_{grp} Ki_red12_{grp})))
             """.format(grp=grp)
         ki_multiplier_change_str = ki_multiplier_change_str + temp_str
 
