bfa12fb26bbae27f4011f5f731c5ec49ca676a2d
ManuelaRunge <manuela.runge@northwestern.edu>
2021-01-20 15:02:26 -0600

    revised sbatch submission script

--- simulation_helpers.py
+++ simulation_helpers.py
@@ -253,6 +253,13 @@ def generateSubmissionFile_quest(scen_num, exp_name, experiment_config, trajecto
              '#SBATCH -N 1\n' \
              '#SBATCH --ntasks-per-node=1\n' \
              '#SBATCH --mem=18G'
+    header_post = '#!/bin/bash\n'\
+                  '#SBATCH -A p30781\n' \
+                  '#SBATCH -p short\n' \
+                  '#SBATCH -t 02:00:00\n' \
+                  '#SBATCH -N 1\n' \
+                  '#SBATCH --ntasks-per-node=1\n' \
+                  '#SBATCH --mem=32G'
     jobname = f'\n#SBATCH	--job-name="{exp_name_short}"'
     array = f'\n#SBATCH --array=1-{str(scen_num)}'
     err = '\n#SBATCH --error=log/arrayJob_%A_%a.err'
@@ -272,9 +279,9 @@ def generateSubmissionFile_quest(scen_num, exp_name, experiment_config, trajecto
     pymodule = '\n\nml python/anaconda3.6\n'
     rmodule = '\n\nml module load R/4.0.0\n'
 
-    pycommand = f'\npython /projects/p30781/covidproject/covid-chicago/nucluster/combine_and_trim.py  --stem "{exp_name}" '
+    pycommand = f'\ncd /projects/p30781/covidproject/covid-chicago/nucluster/\npython combine_and_trim.py  --stem "{exp_name}" '
     file = open(os.path.join(temp_exp_dir, '0_runCombineAndTrimTrajectories.sh'), 'w')
-    file.write(header + jobname + err + out + pymodule + pycommand)
+    file.write(header_post + jobname + err + out + pymodule + pycommand)
     file.close()
 
     pycommand = f'cd {plotters_dir} \npython /projects/p30781/covidproject/covid-chicago/nucluster/cleanup.py --stem "{exp_name}"' \
