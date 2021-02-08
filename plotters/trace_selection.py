285541f42b4de23b7e4f231198d73f9238e22cf4
ManuelaRunge <manuela.runge@northwestern.edu>
2021-02-08 11:53:50 -0600

    edited name of plot from trace selection

--- plotters/trace_selection.py
+++ plotters/trace_selection.py
@@ -160,7 +160,7 @@ def compare_ems(exp_name, ems_nr,first_day,last_day,weights_array,
 
         plot_sim_and_ref(df, ems_nr, ref_df, channels=channels, data_channel_names=data_channel_names, titles=titles,
                          region_label=region_label, first_day=first_day, last_day=last_day, plot_path=plot_path,
-                         plot_name_suffix =f'_best_fit_n{str(n_traces_to_keep)}')
+                         plot_name_suffix =f'_best_fit_{str(1/traces_to_keep_ratio)}')
 
 
 if __name__ == '__main__':
