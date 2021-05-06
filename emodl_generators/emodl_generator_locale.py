2ceacd0c8fffb599837fc13e976a00252f7c137c
Jiayi Gu <57238330+jiayigu123@users.noreply.github.com>
2021-05-05 22:27:04 -0500

    Update emodl_generator_locale.py

--- emodl_generators/emodl_generator_locale.py
+++ emodl_generators/emodl_generator_locale.py
@@ -1259,7 +1259,7 @@ class covidModel:
     
             for i, date in enumerate(intervention_dates, 1):
                 temp_str = f'(time-event ki_mitigation_change{i} {covidModel.DateToTimestep(pd.Timestamp(date), self.startdate)}('
-                temp_str = temp_str + ''.join([f' (Ki_{grp} {ki_reset[i-1]})' for grp in [1,2] ])
+                temp_str = temp_str + ''.join([f' (Ki_{grp} {ki_reset[i-1]})' for grp in  self.grpList] )
                 temp_str = temp_str + f'))\n'
                 emodl_timeevents = emodl_timeevents + temp_str 
                                    
