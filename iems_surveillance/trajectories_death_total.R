2ff71b4fa048ecca2c010d5d0d456b05f20b620e
Jiayi Gu <57238330+Jiayi-Gu@users.noreply.github.com>
2021-05-27 18:49:20 -0500

    Add files via upload

---
+++ iems_surveillance/trajectories_death_total.R
@@ -0,0 +1,18 @@
+#for calculating total amount of deaths for any Set A/B per X1
+setwd("~/Spring 2021")
+traj = read.csv(file = 'trajectoriesDat.csv')
+total_time = max(traj$time)
+rows = nrow(traj)
+reps = rows/total_time
+rowVal=0
+
+deaths_setA = data.frame(matrix(0, nrow =reps,ncol= 1))
+
+
+  
+for (i in 1:reps){
+  print(i)
+  deaths$matrix.0..nrow...reps..ncol...1.[i] = traj$deaths_EMS.11[i*300]
+}
+
+write.csv(deaths, file = "total_death_set.csv")
